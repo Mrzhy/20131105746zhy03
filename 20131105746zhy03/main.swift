83cb5caab2b99845b4270eaace4e2473a16a284c
Mr.Zhao <mister.zhao@foxmail.com>
2016-03-14 19:46:26 +0800

    Initial Commit

---
+++ 20131105746zhy03/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  20131105746zhy03
+//
+//  Created by Mr.Zhao on 16/3/14.
+//  Copyright © 2016年 Mr.Zhao. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
